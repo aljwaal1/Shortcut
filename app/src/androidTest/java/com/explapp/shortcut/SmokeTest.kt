4d771800d74bcb26fb90e48ac6f1e05bd470c380
aljwaal1 <yaya15112016@gmail.com>
2026-09-16 07:47:39 +0300

    test: add Compose launch smoke test

---
+++ app/src/androidTest/java/com/explapp/shortcut/SmokeTest.kt
@@ -0,0 +1,20 @@
+package com.explapp.shortcut
+
+import androidx.compose.ui.test.assertExists
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class SmokeTest {
+    @get:Rule
+    val composeRule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun appLaunches() {
+        composeRule.onNodeWithTag("shortcut_root").assertExists()
+    }
+}
