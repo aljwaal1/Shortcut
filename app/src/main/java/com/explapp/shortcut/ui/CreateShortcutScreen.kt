b7b13528c61ee75d6253dc28030fff186fcde826
aljwaal1 <yaya15112016@gmail.com>
2026-09-16 08:24:15 +0300

    fix: use Compose keyboard options import

--- app/src/main/java/com/explapp/shortcut/ui/CreateShortcutScreen.kt
+++ app/src/main/java/com/explapp/shortcut/ui/CreateShortcutScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.FilterChip
@@ -25,7 +26,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.KeyboardOptions
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.explapp.shortcut.R
