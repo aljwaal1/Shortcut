be41d8bfe326748ecee347660a923819aaec1764
aljwaal1 <yaya15112016@gmail.com>
2026-09-16 07:45:51 +0300

    feat: add main Compose shell

---
+++ app/src/main/java/com/explapp/shortcut/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.explapp.shortcut
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import com.explapp.shortcut.ui.ShortcutApp
+import com.explapp.shortcut.ui.ShortcutTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ShortcutTheme {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .testTag("shortcut_root"),
+                ) {
+                    ShortcutApp()
+                }
+            }
+        }
+    }
+}
