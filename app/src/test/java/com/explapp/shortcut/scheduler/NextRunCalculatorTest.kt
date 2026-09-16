4df4d406f85d925850f1a834bba1a1372fd93f55
aljwaal1 <yaya15112016@gmail.com>
2026-09-16 08:24:22 +0300

    test: define next run calculation

---
+++ app/src/test/java/com/explapp/shortcut/scheduler/NextRunCalculatorTest.kt
@@ -0,0 +1,33 @@
+package com.explapp.shortcut.scheduler
+
+import com.explapp.shortcut.domain.RepeatOption
+import com.explapp.shortcut.domain.ScheduledAppShortcut
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class NextRunCalculatorTest {
+
+    private val zone = ZoneId.of("Europe/Stockholm")
+
+    @Test
+    fun dailyScheduleMovesToTomorrowWhenTimePassed() {
+        val now = ZonedDateTime.of(2026, 9, 16, 9, 0, 0, 0, zone)
+        val shortcut = ScheduledAppShortcut("Maps", "pkg", 7, 30, RepeatOption.DAILY)
+
+        val next = NextRunCalculator.nextRun(now, shortcut)
+
+        assertEquals(ZonedDateTime.of(2026, 9, 17, 7, 30, 0, 0, zone), next)
+    }
+
+    @Test
+    fun weekdaysScheduleSkipsWeekend() {
+        val friday = ZonedDateTime.of(2026, 9, 18, 20, 0, 0, 0, zone)
+        val shortcut = ScheduledAppShortcut("Maps", "pkg", 7, 30, RepeatOption.WEEKDAYS)
+
+        val next = NextRunCalculator.nextRun(friday, shortcut)
+
+        assertEquals(ZonedDateTime.of(2026, 9, 21, 7, 30, 0, 0, zone), next)
+    }
+}
