c38849f3167f909e7802621bea5998b7c8c45376
aljwaal1 <yaya15112016@gmail.com>
2026-09-17 08:00:24 +0300

    feat: publish dynamic launcher shortcuts

--- app/src/main/java/com/explapp/shortcut/MainActivity.kt
+++ app/src/main/java/com/explapp/shortcut/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import com.explapp.shortcut.system.DynamicShortcutPublisher
 import com.explapp.shortcut.tools.ToolHubScreen
 import com.explapp.shortcut.tools.ToolRouter
 import com.explapp.shortcut.ui.ShortcutApp
@@ -19,6 +20,7 @@ import com.explapp.shortcut.ui.ShortcutTheme
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        DynamicShortcutPublisher.publish(this)
         setContent {
             ShortcutTheme {
                 var showTools by remember { mutableStateOf(false) }
