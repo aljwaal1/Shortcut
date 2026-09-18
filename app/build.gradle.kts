d7208894e68b6c6c5d9d37e3506f6d7467ea8eb9
aljwaal1 <yaya15112016@gmail.com>
2026-09-18 11:18:17 +0300

    chore: bump expanded automation build to 0.8.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.explapp.shortcut"
         minSdk = 26
         targetSdk = 36
-        versionCode = 16
-        versionName = "0.8.1"
+        versionCode = 17
+        versionName = "0.8.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
