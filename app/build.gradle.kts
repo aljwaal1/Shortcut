0e204ed64e80d9cf8751972e9d61756bd5911d62
aljwaal1 <yaya15112016@gmail.com>
2026-09-18 14:06:42 +0300

    chore: bump full schedule screenshot Telegram build to 0.8.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.explapp.shortcut"
         minSdk = 26
         targetSdk = 36
-        versionCode = 22
-        versionName = "0.8.7"
+        versionCode = 23
+        versionName = "0.8.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
