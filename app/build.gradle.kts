cdd81978804a6ed57cb00eb4ca4b81fc134f8e3f
aljwaal1 <yaya15112016@gmail.com>
2026-09-18 13:33:58 +0300

    chore: bump verified screenshot telegram flow to 0.8.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.explapp.shortcut"
         minSdk = 26
         targetSdk = 36
-        versionCode = 20
-        versionName = "0.8.5"
+        versionCode = 21
+        versionName = "0.8.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
