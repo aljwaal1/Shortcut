370f9a1bdbc7211262b94f0195385519c8b89ef8
aljwaal1 <yaya15112016@gmail.com>
2026-09-19 08:51:36 +0300

    chore: bump Telegram bot hardening build to 0.8.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.explapp.shortcut"
         minSdk = 26
         targetSdk = 36
-        versionCode = 27
-        versionName = "0.8.12"
+        versionCode = 28
+        versionName = "0.8.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
