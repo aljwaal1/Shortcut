17e72ca43ee4a5e7c9d3ec4d9fddcefe8a831cba
aljwaal1 <yaya15112016@gmail.com>
2026-09-19 17:29:01 +0300

    chore: bump Telegram destination UX build to 0.8.15

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.explapp.shortcut"
         minSdk = 26
         targetSdk = 36
-        versionCode = 29
-        versionName = "0.8.14"
+        versionCode = 30
+        versionName = "0.8.15"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
