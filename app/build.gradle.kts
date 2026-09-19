22fe152412f07dfef0c271369080e4fead410ee0
aljwaal1 <yaya15112016@gmail.com>
2026-09-19 17:22:41 +0300

    chore: bump crash-hardening build to 0.8.14

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.explapp.shortcut"
         minSdk = 26
         targetSdk = 36
-        versionCode = 28
-        versionName = "0.8.13"
+        versionCode = 29
+        versionName = "0.8.14"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
