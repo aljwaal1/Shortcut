afe5b760609221c8454ea698f48eec6fc9a6fb2f
aljwaal1 <yaya15112016@gmail.com>
2026-09-19 17:32:58 +0300

    chore: bump Telegram username-first build to 0.8.16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.explapp.shortcut"
         minSdk = 26
         targetSdk = 36
-        versionCode = 30
-        versionName = "0.8.15"
+        versionCode = 31
+        versionName = "0.8.16"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
