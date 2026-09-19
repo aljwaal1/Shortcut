0c9c5e53ca3977894a9fca7341d448b11a048834
aljwaal1 <yaya15112016@gmail.com>
2026-09-19 06:28:39 +0300

    chore: bump Telegram automation fixes to 0.8.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.explapp.shortcut"
         minSdk = 26
         targetSdk = 36
-        versionCode = 25
-        versionName = "0.8.10"
+        versionCode = 26
+        versionName = "0.8.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
